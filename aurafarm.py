dc0baf6917faf835dc0946629c614dbce4bc098d
Kozári Armand <b3kozarm@vasvari.org>
2025-09-29 14:22:30 +0200

    Második feladat

---
+++ aurafarm.py
@@ -0,0 +1,11 @@
+a=int(input("Kérem a téglalap A oldalának a hosszát!"))
+b=int(input("Kérem a téglalap B oldalának a hosszát!"))
+
+T=a*b
+K=2*(a+b)
+
+print("A téglalap egyik oldaal:",a)
+print("A téglalap másik oldala:",b)
+
+print("A téglalap területe: ", T)
+print("A téglalap kerülete:",K)
